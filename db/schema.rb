32038d1d46d1a2b91c69cfcfe5b04376c8955c7e
cramsel <Luxicorde@gmail.com>
2022-02-17 20:58:48 -0600

    Add Post model and has many/belongs to for posts and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_18_015954) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_18_024546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "audio_url"
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,23 @@ User.create(username: "NotPeter", email: "test@test.com", first_name: "Peter", l
 User.create(username: "Siames", email: "test2@test.com", first_name: "", last_name: "", description: "Argentinian band", password: "password")
 User.create(username: "Luxi", email: "test3@test.com", first_name: "Calum", last_name: "Amsel", description: "Wannabe Composer", password: "password")
 
+Post.create(
+  user_id: 3,
+  audio_url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-15.mp3",
+  title: "Playing around with procedural stuff",
+  description: "Found a new program to make procedural music, thanks Java",
+)
+Post.create(
+  user_id: 2,
+  audio_url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+  title: "SoundHelix 1",
+  description: "",
+)
+Post.create(
+  user_id: 2,
+  audio_url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+  title: "SoundHelix 4",
+  description: "",
+)
+
 puts "Seeds planted"
