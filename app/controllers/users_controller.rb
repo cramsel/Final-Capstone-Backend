0f937d526c199dd6dff18fae2389dbf5c306bffe
cramsel <Luxicorde@gmail.com>
2022-02-22 17:17:14 -0600

    Add working show route for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
     user = User.all
     render json: user
   end
+
+  def show
+    user = User.find_by(id: params[:id])
+    render json: user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/users" => "users#index"
+  get "/users/:id" => "users#show"
 end
