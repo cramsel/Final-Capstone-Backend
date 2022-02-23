2f4eb9b8694bd4a9e9fa25f95e166a762691db5b
cramsel <Luxicorde@gmail.com>
2022-02-23 17:51:44 -0600

    Add working create user route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,21 @@ class UsersController < ApplicationController
     user = User.find_by(id: params[:id])
     render json: user
   end
+
+  def create
+    user = User.new(
+      username: params[:username],
+      email: params[:email],
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      description: params[:description],
+      password: params[:password], #this is a ruby thing
+      password_confirmation: params[:password_confirmation],
+    )
+    if user.save
+      render json: { message: "User created" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
 end
